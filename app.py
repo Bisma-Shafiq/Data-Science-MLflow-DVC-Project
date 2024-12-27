364cef4b57cbc6032bb82b7ff79b5c7b2cf03c64
Bisma Shafiq <bismashafiq26@gmail.com>
2024-12-27 15:09:44 +0500

    exception.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from src.mlproject.logger import logging
+from src.mlproject.exception_handling import CustomException
+import sys
+
+
 
 if __name__ == "__main__":
     logging.info("Hello, world!")
+
+    try:
+        a=1/0
+
+    except Exception as e:
+        logging.info("An error occurred")
+        raise CustomException(e,sys)
