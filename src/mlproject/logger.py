a41189fe99235176e233e8e2987bb6041ad3d325
Bisma Shafiq <bismashafiq26@gmail.com>
2024-12-27 14:47:39 +0500

    logger.py

--- src/mlproject/logger.py
+++ src/mlproject/logger.py
@@ -1,12 +1,12 @@
 import os
 import logging
-from datatime import datetime
+from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d')}.log"
 # lof file path
 log_path = os.path.join(os.getcwd(),"logs", LOG_FILE)
 # make sure the directory exists
-os.makedirs(log_path,exit_ok=True)
+os.makedirs(log_path,exist_ok=True)
 # complete log file path
 LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
 
