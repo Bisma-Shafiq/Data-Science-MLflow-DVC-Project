9012bd0703c71aecdda02dd50bf4cc2d17982ad5
Bisma Shafiq <bismashafiq26@gmail.com>
2024-12-28 18:27:29 +0500

    utilis.py

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -6,6 +6,10 @@ from src.mlproject.exception_handling import CustomException
 from src.mlproject.logger import logging
 from dotenv import load_dotenv
 import pymysql
+
+import numpy as np
+
+import pickle
 load_dotenv()
 
 host = os.getenv("host")
@@ -31,3 +35,17 @@ def sql_data_read():
     except Exception as e:
         logging.info("An error occurred")
         raise CustomException(e,sys)
+
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open (file_path,'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+
+            
+    except Exception as e:
+        raise CustomException (e,sys)
